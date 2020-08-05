c027b624c5065811e20bcf4ff5c2dd1f7802f80d
tomertv2 <67857440+tomertv2@users.noreply.github.com>
2020-08-05 20:31:46 +0300

    load list

--- clientside.js
+++ clientside.js
@@ -1,6 +1,19 @@
 const loadData = async () => {
     const { data } = await axios.get(`http://localhost:3001/products`);
-    console.log(data) 
+    startList(data);
+};
+
+const listDiv = document.getElementById("list");
+
+const startList = (list) => {
+    let htmlText = ``;
+    
+    list.forEach(item => {
+        htmlText += `
+            <div>${item.id} ${item.name}</div>`;
+    });
+
+    listDiv.innerHTML = htmlText;
 };
 
 loadData();
