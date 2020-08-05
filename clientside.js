c5b4b9462f6f9b337ae1a7607c0924aa3ee2e595
tomertv2 <67857440+tomertv2@users.noreply.github.com>
2020-08-05 23:04:24 +0300

    post new item

--- clientside.js
+++ clientside.js
@@ -1,19 +1,37 @@
-const loadData = async () => {
+const startList = async () => {
     const { data } = await axios.get(`http://localhost:3001/products`);
-    startList(data);
+    loadData(data);
+    console.log(data);
+};
+
+const addItem = async (id, item) => {
+    const { data } = await axios.post(`http://localhost:3001/products`, {id: id, name: item})
+    .then(addToDiv(id, item));
+    console.log(data)
 };
 
 const listDiv = document.getElementById("list");
 
-const startList = (list) => {
+const idText = document.getElementById("id");
+const itemText = document.getElementById("item");
+
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", () => addItem(idText.value, itemText.value));
+
+const loadData = (list) => {
     let htmlText = ``;
     
     list.forEach(item => {
-        htmlText += `
-            <div>${item.id} ${item.name}</div>`;
+        htmlText += `<div>${item.id} ${item.name} <button class="delete">Delete</button></div>`;
     });
 
     listDiv.innerHTML = htmlText;
 };
 
-loadData();
+const addToDiv = (id, item) => {
+    const htmlText = `<div>${id} ${item} <button class="delete">Delete</button></div></div>`;
+
+    listDiv.innerHTML += htmlText;
+};
+
+startList();
