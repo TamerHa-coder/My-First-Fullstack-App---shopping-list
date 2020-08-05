2131db9129e63bdcd537ff9c5a7530550eb5e9c4
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-05 15:00:16 +0300

    adding get, put, post and delete

--- rest.js
+++ rest.js
@@ -8,63 +8,52 @@ function logger (req, res, next){
     next();
 }
 
-let posts = [
-    {
-        id: 'f43f',
-        author: 'Tamer',
-        content: 'Very good post'
+let items = [
+    { 
+        id : '1' ,
+        name: 'milk'
     },
     {
-        id: 'f56f',
-        author: 'Dor',
-        content: 'Very bad post'
+        id : '2',
+        name: 'eggs'
+    },
+    {
+        id : '3',
+        name: 'bread'
     }
-];
+    ];
 
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+app.get('/products', (req, res) => {
+    res.send(items);
 });
 
-app.get('/post', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/post', (req, res) => {
-    posts.push(req.body);
-    res.send(req.body)
-})
-
-app.get('/post/:id', (req, res) => {
-    for(let post of posts){
-        if(post.id === req.params.id){
-            res.send(post);
-        }
+app.get('/products/:id', (req, res) => {
+    for(let item of items){
+        if(item.id === req.params.id){
+            res.send(item);
+        }  
     }
-})
+});
 
-
-app.put('/post/:id', (req, res) => {
-
-    posts.forEach((post, i) =>{
-    for(let post of posts){
-            if(post.id === req.params.id){
-                post[i] = req.body;
+app.put('/products/:id', (req, res) => {
+    items.forEach((item, i) =>{
+    for(let item of items){
+            if(item.id === req.params.id){
+                item[i] = req.body;
                 res.send(req.body);
+            }
     }
-   
-        }
     }
 )}
 );
 
-app.delete('/post/:id', (req, res) => {
-
-    posts.forEach((post, i) =>{
-    for(let post of posts){
-            if(post.id === req.params.id){
-                posts.splice(i, 1);
-                res.send(req.params.id + ' deleted');
+app.delete('/products/:id', (req, res) => {
+    items.forEach((item, i) =>{
+    for(let item of items){
+            if(item.id === req.params.id){
+                items.splice(i,1);
+                res.send();
     }
    
         }
